ac01ce63bd16c8e784c05a94c39e74b74846d194
Brendan Kumasaka <bhk22@hawaii.edu>
2023-10-11 19:03:58 -1000

    problem set completed

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-   
+    
     @IBOutlet weak var tableView: UITableView!
     var myFriendsArray = ["Sara", "Nicole", "Grant"]
     
@@ -18,20 +18,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                      
-                      let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-                      let text = myFriendsArray[indexPath.row]
-                      cell.textLabel?.text = text
-                      return cell
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = myFriendsArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+        
+        
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+       
+    @IBOutlet var tableView: UITableView!
+    var myFavoriteCountries = ["Japan", "France", "Australia"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFavoriteCountries.count
+        
+            }
+            
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let text = myFavoriteCountries[indexPath.row]
+            cell.textLabel?.text = text
+            return cell
+            }
+            
+            
+        }
+        
+        func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        
+        
     }
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
